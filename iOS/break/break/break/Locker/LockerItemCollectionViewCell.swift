e723cd8e3b689a9ca1a7eae654515da85866fa82
Saagar Jha <saagar@saagarjha.com>
2018-09-27 23:31:23 -0700

    Update for iOS 12

--- iOS/break/break/break/Locker/LockerItemCollectionViewCell.swift
+++ iOS/break/break/break/Locker/LockerItemCollectionViewCell.swift
@@ -11,12 +11,4 @@ import UIKit
 class LockerItemCollectionViewCell: UICollectionViewCell {
 	@IBOutlet weak var typeImageView: UIImageView!
 	@IBOutlet weak var nameLabel: UILabel!
-	@IBOutlet weak var typeImageViewWidthConstraint: NSLayoutConstraint! {
-		didSet {
-			// Not sure why this is necessary, but adding it makes AutoLayout
-			// stop complaining
-			contentView.translatesAutoresizingMaskIntoConstraints = false
-			nameLabel.preferredMaxLayoutWidth = typeImageViewWidthConstraint.constant
-		}
-	}
 }
